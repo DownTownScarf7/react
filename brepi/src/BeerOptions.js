7d6f91a1e60c234e91cdee2b9fd9bb0c9b936298
DownTownScarf7 <vbence741@gmail.com>
2018-08-02 14:23:28 +0200

    class => function transformation

--- brepi/src/BeerOptions.js
+++ brepi/src/BeerOptions.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class BeerOptions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      beerCount: 6,
-    };
+export default function BeerOptions(props) {
+  const sendOnClick = () => {
+    console.log(props.beerCount);
   }
 
-  beerCountOnChange = event => {
-    this.setState({beerCount: +event.target.value});
-  }
-
-  sendOnClick = () => {
-    console.log(this.state.beerCount);
-  }
-
-  render() {
-    return (
-      <div className="options-wrapper">
-        <input className="beerSearch" type="text" autoFocus />
-        <input className="beerCount" type="range" min="6" max="20" value={this.state.beerCount} onChange={this.beerCountOnChange}/>
-        <button className="btn send" onClick={this.sendOnClick} >OK</button>
-      </div>
-    );
-  }
+  return (
+    <div className="options-wrapper">
+      <input className="beerSearch" type="text" autoFocus />
+      <input className="beerCount" type="range" min="6" max="20" value={props.beerCount} onChange={props.beerCountOnChange}/>
+      <button className="btn send" onClick={sendOnClick} >OK</button>
+    </div>
+  );
 }
